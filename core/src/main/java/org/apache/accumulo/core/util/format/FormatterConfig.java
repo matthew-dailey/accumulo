1e2d8b3f4caed6a101f63af723ce8f9715447bd9
Matt Dailey <matthew.dailey1@gmail.com>
2015-12-04 21:50:58 -0500

    ACCUMULO-2493: Added FormatterConfig (and test)

---
+++ core/src/main/java/org/apache/accumulo/core/util/format/FormatterConfig.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.accumulo.core.util.format;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Holds configuration settings for a {@link Formatter}
+ */
+public class FormatterConfig {
+
+  private boolean printTimestamps;
+  private int shownLength;
+
+  public FormatterConfig() {
+    this.setPrintTimestamps(false);
+    this.doNotLimitShowLength();
+  }
+
+  public boolean willPrintTimestamps() {
+    return printTimestamps;
+  }
+
+  public FormatterConfig setPrintTimestamps(boolean printTimestamps) {
+    this.printTimestamps = printTimestamps;
+    return this;
+  }
+
+  public int getShownLength() {
+    return shownLength;
+  }
+
+  public boolean willLimitShowLength() {
+    return this.shownLength != Integer.MAX_VALUE;
+  }
+
+  /**
+   * If given a negative number, throws an {@link IllegalArgumentException}
+   *
+   * @param shownLength maximum length of formatted output
+   * @return {@code this} to allow chaining of set methods
+   */
+  public FormatterConfig setShownLength(int shownLength) {
+    Preconditions.checkArgument(shownLength >= 0, "Shown length cannot be negative");
+    this.shownLength = shownLength;
+    return this;
+  }
+
+  public FormatterConfig doNotLimitShowLength() {
+    this.shownLength = Integer.MAX_VALUE;
+    return this;
+  }
+}
